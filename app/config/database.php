b3719f378ed1949734b82a11bd505ca64364be46
jhonerick31 <jhonerickrecaro41@gmail.com>
2025-10-08 22:57:56 +0800

    k

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> getenv('DB_HOST'),
-    'port'		=> getenv('DB_PORT'),
-    'username'	=> getenv('DB_USERNAME'),
-    'password'	=> getenv('DB_PASS'),
-    'database'	=> getenv('DB_NAME'),
+    'hostname'	=> getenv("DB_HOST"),
+    'port'		=> getenv("DB_PORT"),
+    'username'	=> getenv("DB_USERNAME"),
+    'password'	=> getenv("DB_PASS"),
+    'database'	=> getenv("DB_NAME"),
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
